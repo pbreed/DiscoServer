e24befc4f3edccee9583daed3e31cc5492070bb3
Paul Breed <pbreed@netburner.com>
2017-09-21 14:16:57 -0700

    Trying to split server

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
-app.get('/', (req, res) => {
+app.get('/register', (req, res) => {
   var now=new Date();
-  res.send('Version4 shown at '+now.toString())
+  res.send('Version 4 get shown at '+now.toString())
+})
+app.post('/post', (req, res) => {
+  var now=new Date();
+  res.send('Version 5 post shown at '+now.toString())
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
 
