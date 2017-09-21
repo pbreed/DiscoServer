f735271b3cb168900637673621dcc933365070f4
Paul Breed <pbreed@netburner.com>
2017-09-21 15:33:15 -0700

    stringify

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ r2.ip='10.1.1.75';
 r2.mac='00:11:22:33:44:55';
 r2.time=new Date();
 a1=[r1,r2];
-res.json('{"result:"'+JSON.stringify(a1)+'}');
+res.json('{\"RESULT\":'+JSON.stringify(a1)+'}');
 })
 app.get('/register', (req, res) => {
   var now=new Date();
