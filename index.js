d4c83a40fb3de42838938be520b1337747e6402f
Paul Breed <pbreed@netburner.com>
2017-09-21 14:08:46 -0700

    Testing again

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+app.use(express.static('public'))
 app.get('/', (req, res) => {
   var now=new Date();
-  res.send('Version3 shown at '+now.toString())
+  res.send('Version4 shown at '+now.toString())
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
+
