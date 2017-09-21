b51fca13fca5b5be52b0eef28b417abe545e6db0
Paul Breed <pbreed@netburner.com>
2017-09-21 15:13:40 -0700

    Trying to make json

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ app.use(express.static('public'))
 app.get('/jtest', (req, res) => {
 app.set('json spaces', 40);
 r1={};
-r1.t1='t1';
-r1.t2='t2';
+r1.t1='Fred';
+r1.t2='Bob';
 r1.t3=1234;
-res.json(r1)
+
+r2={};
+r2.t1='Bill';
+r2.t2='George';
+r2.t3=999;
+
+a1=[r1,r2];
+
+
+res.json(a1)
 })
 app.get('/register', (req, res) => {
   var now=new Date();
