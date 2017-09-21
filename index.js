83ba80c22199045faedccbd7ab0253126b433a1a
Paul Breed <pbreed@netburner.com>
2017-09-21 14:55:58 -0700

    Trying to make json

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
+
+app.get('/jtest', (req, res) => {
+app.set('json spaces', 40);
+res.json(req.toString())
+})
 app.get('/register', (req, res) => {
   var now=new Date();
   res.send('Version 4 get shown at '+now.toString()+' from '+req.get('X-Real-IP').toString())
