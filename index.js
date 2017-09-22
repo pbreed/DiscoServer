49155e793e1af208a4e6d6b4add5bee6a0357f5e
Paul Breed <pbreed@netburner.com>
2017-09-22 10:25:17 -0700

    Trying to add parsing to post process

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/register', (req, res) => {
   var now=new Date();
   res.send('Version 4 get shown at '+now.toString()+' from '+req.get('X-Real-IP').toString())
 })
+
 app.post('/post', (req, res) => {
   response_obj ={};
   response_obj.LocalIp = req.body.LocalIp;
@@ -31,8 +32,24 @@ app.post('/post', (req, res) => {
   response_obj.AppName=req.body.AppName;
   response_obj.Platform=req.body.Platform;
   response_obj.when=new Date();
-  res.json(response_obj);
+  res.send(' Tried to parse');
 })
+
+app.post('/post2', (req, res) => {
+r1={};
+r1.ip='10.1.1.86';
+r1.mac='00:01:02:03:04:05';
+r1.time=new Date();
+
+r2={};
+r2.ip='10.1.1.75';
+r2.mac='00:11:22:33:44:55';
+r2.time=new Date();
+a1=[r1,r2];
+result={a1};
+res.json(result);
+})
+
 
 app.listen(3000, () => console.log('Server running on port 3000'))
 
