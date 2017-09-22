3a8430681dc7230e62a8813e5475e78922a4c546
Paul Breed <pbreed@netburner.com>
2017-09-22 11:11:33 -0700

    comming from batch file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.json())
+var LastPost={};
+
 
 app.get('/jtest', (req, res) => {
 /*app.set('json spaces', 40);*/
@@ -21,8 +23,9 @@ result={a1};
 res.json(result);
 })
 app.get('/register', (req, res) => {
-  var now=new Date();
-  res.send('Version 4 get shown at '+now.toString()+' from '+req.get('X-Real-IP').toString())
+  res.json(LastPost);
+  //var now=new Date();
+  //res.send('Version 4 get shown at '+now.toString()+' from '+req.get('X-Real-IP').toString())
 })
 
 app.post('/post', (req, res) => {
@@ -34,25 +37,10 @@ app.post('/post', (req, res) => {
   response_obj.Platform=req.body.Platform;
   response_obj.when=new Date();
   result={response_obj};
+  LastPost=result;
   res.json(result);
 //  res.send(' Tried to parse'+ response_obj.when.toString()+ 'IP'+response_obj.LocalIp);
 })
-
-app.post('/post2', (req, res) => {
-r1={};
-r1.ip='10.1.1.86';
-r1.mac='00:01:02:03:04:05';
-r1.time=new Date();
-
-r2={};
-r2.ip='10.1.1.75';
-r2.mac='00:11:22:33:44:55';
-r2.time=new Date();
-a1=[r1,r2];
-result={a1};
-res.json(result);
-})
-
 
 app.listen(3000, () => console.log('Server running on port 3000'))
 
